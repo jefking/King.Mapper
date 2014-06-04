c40f7578aabe0d004d73f74446dab3987692c02c
Jef King <jamieking80@gmail.com>
2014-06-03 20:11:42 -0700

    test cases

---
+++ King.Mapper.Tests/Data/ObjectTests.cs
@@ -0,0 +1,31 @@
+﻿namespace King.Mapper.Tests.Data
+{
+    using King.Mapper.Data;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System;
+
+    [TestClass]
+    public class ObjectTests
+    {
+        [TestMethod]
+        public void IsNotNullObjectNull()
+        {
+            object obj = null;
+            Assert.IsFalse(obj.IsNotNull());
+        }
+        
+        [TestMethod]
+        public void IsNotNullObjectNotNull()
+        {
+            var obj = new object();
+            Assert.IsTrue(obj.IsNotNull());
+        }
+
+        [TestMethod]
+        public void IsNotNullObjectDBNull()
+        {
+            var obj = DBNull.Value;
+            Assert.IsFalse(obj.IsNotNull());
+        }
+    }
+}
