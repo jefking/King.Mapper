d72b546193e0ce2e3bc52ad3940b80f49ccc6a2a
Jef King <jamieking80@gmail.com>
2014-06-10 20:28:46 -0700

    more test cases major revision
    First Stable Version to land.

--- King.Mapper.Tests/Data/IDataReaderTests.cs
+++ King.Mapper.Tests/Data/IDataReaderTests.cs
@@ -1,9 +1,14 @@
 ﻿namespace King.Mapper.Tests.Data
 {
     using King.Mapper.Data;
+    using King.Mapper.Tests.Models;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using NSubstitute;
     using System;
+    using System.Linq;
+    using System.Collections.Generic;
     using System.Data;
+    using System.Text;
 
     [TestClass]
     public class IDataReaderTests
@@ -30,6 +35,30 @@
         {
             IDataReader reader = null;
             reader.GetFieldNames();
+        }
+
+        [TestMethod]
+        public void GetFieldNames()
+        {
+            var random = new Random();
+            var columns = new List<string>();
+            var total = random.Next(25) + 1;
+            var reader = Substitute.For<IDataReader>();
+            reader.FieldCount.Returns(total);
+            for (var i = 0; i < total; i++)
+            {
+                var column = Guid.NewGuid().ToString();
+                columns.Add(column);
+                reader.GetName(i).Returns(column);
+            }
+            
+            var fields = reader.GetFieldNames();
+
+            for (var i = 0; i < total; i++)
+            {
+                reader.Received().GetName(i);
+                Assert.AreEqual(columns[i], fields[i]);
+            }
         }
     }
 }
