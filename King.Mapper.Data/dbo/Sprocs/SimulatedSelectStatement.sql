8b9ccbaf0b197cd5a7255a3d305c02bdf5a1d2de
Jef King <jamieking80@gmail.com>
2014-06-21 11:23:53 -0700

    Adding more data types

--- King.Mapper.Data/dbo/Sprocs/SimulatedSelectStatement.sql
+++ King.Mapper.Data/dbo/Sprocs/SimulatedSelectStatement.sql
@@ -1,8 +1,36 @@
 ﻿CREATE PROCEDURE [dbo].[SimulatedSelectStatement]
 	@TestInt int = NULL
+	, @TestBigInt bigint = NULL
+	, @TestBit bit = NULL
+	, @TestDecimal decimal = NULL
+	, @TestMoney money = NULL
+	, @TestFloat float = NULL
+	, @TestDate date = NULL
+	, @TestDateTime2 datetime2 = NULL
+	, @TestDateTime datetime = NULL
+	, @TestChar char = NULL
+	, @TestText text = NULL
+	, @TestNChar nchar = NULL
+	, @TestNText ntext = NULL
+	, @TestBinary binary = NULL
+	, @TestImage image = NULL
 AS
 BEGIN
 
 	SELECT @TestInt AS 'Identifier'
+		, @TestBigInt AS 'BigInt'
+		, @TestBit AS 'Bit'
+		, @TestDecimal AS 'Decimal'
+		, @TestMoney AS 'Money'
+		, @TestFloat AS 'Float'
+		, @TestDate AS 'Date'
+		, @TestDateTime2 AS 'DateTime2'
+		, @TestDateTime AS 'DateTime'
+		, @TestChar AS 'Char'
+		, @TestText AS 'Text'
+		, @TestNChar AS 'NChar'
+		, @TestNText AS 'Text'
+		, @TestBinary AS 'Binary'
+		, @TestImage AS 'Image'
 
 END
--- King.Mapper.Integration/Model/SimulatedSelectStatement.cs
+++ King.Mapper.Integration/Model/SimulatedSelectStatement.cs
@@ -1,6 +1,7 @@
 ﻿namespace King.Mapper.Integration.Model
 {
     using King.Mapper.Data.Sql;
+using System;
 
     public class SimulatedSelectStatement : IStoredProcedure
     {
@@ -11,7 +12,77 @@
                 return "[dbo].[SimulatedSelectStatement]";
             }
         }
-        public int? TestInt
+        public int TestInt
+        {
+            get;
+            set;
+        }
+        public long TestBigInt
+        {
+            get;
+            set;
+        }
+        public bool TestBit
+        {
+            get;
+            set;
+        }
+        public decimal TestDecimal
+        {
+            get;
+            set;
+        }
+        public decimal TestMoney
+        {
+            get;
+            set;
+        }
+        public float TestFloat
+        {
+            get;
+            set;
+        }
+        public DateTime TestDate
+        {
+            get;
+            set;
+        }
+        public DateTime TestDateTime2
+        {
+            get;
+            set;
+        }
+        public DateTime TestDateTime
+        {
+            get;
+            set;
+        }
+        public char TestChar
+        {
+            get;
+            set;
+        }
+        public string TestText
+        {
+            get;
+            set;
+        }
+        public char TestNChar
+        {
+            get;
+            set;
+        }
+        public string TestNText
+        {
+            get;
+            set;
+        }
+        public byte[] TestBinary
+        {
+            get;
+            set;
+        }
+        public byte[] TestImage
         {
             get;
             set;
