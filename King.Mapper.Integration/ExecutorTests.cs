9803727f87555c712d8b0129c72ef33eead96e0c
Jef King <jamieking80@gmail.com>
2014-06-20 23:01:02 -0700

    minimal integration testing happening

--- King.Mapper.Integration/ExecutorTests.cs
+++ King.Mapper.Integration/ExecutorTests.cs
@@ -1,5 +1,6 @@
 ﻿namespace King.Mapper.Integration
 {
+    using King.Mapper.Data;
     using King.Mapper.Data.Sql;
     using King.Mapper.Integration.Model;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -10,7 +11,9 @@
     [TestClass]
     public class ExecutorTests
     {
+        #region Members
         private readonly string connectionString = ConfigurationManager.AppSettings["database"];
+        #endregion
 
         [TestMethod]
         public async void Insert()
@@ -24,13 +27,29 @@
 
             var executor = new Executor(con);
             var results = await executor.NonQuery(sproc);
+
             Assert.AreEqual(1, results);
         }
 
         [TestMethod]
-        public void Select()
+        public async void Select()
         {
+            var random = new Random();
             var con = new SqlConnection(connectionString);
+            var sproc = new SimulatedSelectStatement()
+            {
+                TestInt = random.Next(),
+            };
+
+            var executor = new Executor(con);
+            var data = await executor.Execute(sproc);
+
+            Assert.IsNotNull(data);
+
+            var obj = data.LoadObject<SelectData>();
+
+            Assert.IsNotNull(obj);
+            Assert.AreEqual(sproc.TestInt, obj.Identifier);
         }
     }
 }
