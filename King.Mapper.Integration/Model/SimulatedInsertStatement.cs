c3a0683cc6ea7cbb08808008df1d1c7af1e2b814
Jef King <jamieking80@gmail.com>
2014-06-20 22:39:26 -0700

    test classes

---
+++ King.Mapper.Integration/Model/SimulatedInsertStatement.cs
@@ -0,0 +1,20 @@
+﻿namespace King.Mapper.Integration.Model
+{
+    using King.Mapper.Data.Sql;
+
+    public class SimulatedInsertStatement : IStoredProcedure
+    {
+        public string Name
+        {
+            get
+            {
+                return "[dbo].[SimulatedInsertStatement]";
+            }
+        }
+        public int? TestInt
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ King.Mapper.Integration/Model/SimulatedSelectStatement.cs
@@ -0,0 +1,25 @@
+﻿namespace King.Mapper.Integration.Model
+{
+    using King.Mapper.Data.Sql;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class SimulatedSelectStatement : IStoredProcedure
+    {
+        public string Name
+        {
+            get
+            {
+                return "[dbo].[SimulatedSelectStatement]";
+            }
+        }
+        public int? TestInt
+        {
+            get;
+            set;
+        }
+    }
+}
