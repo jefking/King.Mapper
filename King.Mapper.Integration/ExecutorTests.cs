baa463f88fba462e2b06da81955b70811d567529
Jef King <jamieking80@gmail.com>
2014-06-21 12:46:38 -0700

    db cleanup

--- King.Mapper.Integration/ExecutorTests.cs
+++ King.Mapper.Integration/ExecutorTests.cs
@@ -19,47 +19,51 @@
         public async void Insert()
         {
             var random = new Random();
-            var con = new SqlConnection(connectionString);
-            var sproc = new SimulatedInsertStatement()
+            using (var con = new SqlConnection(connectionString))
             {
-                TestInt = random.Next(),
-            };
+                var sproc = new SimulatedInsertStatement()
+                {
+                    TestInt = random.Next(),
+                };
 
-            var executor = new Executor(con);
-            var results = await executor.NonQuery(sproc);
+                var executor = new Executor(con);
+                var results = await executor.NonQuery(sproc);
 
-            Assert.AreEqual(1, results);
+                Assert.AreEqual(1, results);
+            }
         }
 
         [TestMethod]
         public async void Select()
         {
-            var con = new SqlConnection(connectionString);
-            var sproc = SimulatedSelectStatement.Create();
+            using (var con = new SqlConnection(connectionString))
+            {
+                var sproc = SimulatedSelectStatement.Create();
 
-            var executor = new Executor(con);
-            var data = await executor.Execute(sproc);
+                var executor = new Executor(con);
+                var data = await executor.Execute(sproc);
 
-            Assert.IsNotNull(data);
+                Assert.IsNotNull(data);
 
-            var obj = data.LoadObject<SelectData>();
+                var obj = data.LoadObject<SelectData>();
 
-            Assert.IsNotNull(obj);
-            Assert.AreEqual<int>(sproc.TestInt, obj.Identifier);
-            Assert.AreEqual<long>(sproc.TestBigInt, obj.BigInt);
-            Assert.AreEqual<bool>(sproc.TestBit, obj.Bit);
-            Assert.AreEqual<DateTime>(sproc.TestDate.Date, obj.Date.Date);
-            Assert.AreEqual<DateTime>(sproc.TestDateTime, obj.DateTime);
-            Assert.AreEqual<DateTime>(sproc.TestDateTime2, obj.DateTime2);
-            Assert.AreEqual<decimal>(sproc.TestDecimal, obj.Decimal);
-            Assert.AreEqual<float>(sproc.TestFloat, obj.Float);
-            Assert.AreEqual<decimal>(sproc.TestMoney, obj.Money);
-            Assert.AreEqual<char>(sproc.TestNChar, obj.NChar);
-            Assert.AreEqual<string>(sproc.TestNText, obj.NText);
-            Assert.AreEqual<string>(sproc.TestText, obj.Text);
-            Assert.AreEqual<byte[]>(sproc.TestBinary, obj.Binary);
-            Assert.AreEqual<byte[]>(sproc.TestImage, obj.Image);
-            Assert.AreEqual<Guid>(sproc.TestGuid, obj.Unique);
+                Assert.IsNotNull(obj);
+                Assert.AreEqual<int>(sproc.TestInt, obj.Identifier);
+                Assert.AreEqual<long>(sproc.TestBigInt, obj.BigInt);
+                Assert.AreEqual<bool>(sproc.TestBit, obj.Bit);
+                Assert.AreEqual<DateTime>(sproc.TestDate.Date, obj.Date.Date);
+                Assert.AreEqual<DateTime>(sproc.TestDateTime, obj.DateTime);
+                Assert.AreEqual<DateTime>(sproc.TestDateTime2, obj.DateTime2);
+                Assert.AreEqual<decimal>(sproc.TestDecimal, obj.Decimal);
+                Assert.AreEqual<float>(sproc.TestFloat, obj.Float);
+                Assert.AreEqual<decimal>(sproc.TestMoney, obj.Money);
+                Assert.AreEqual<char>(sproc.TestNChar, obj.NChar);
+                Assert.AreEqual<string>(sproc.TestNText, obj.NText);
+                Assert.AreEqual<string>(sproc.TestText, obj.Text);
+                Assert.AreEqual<byte[]>(sproc.TestBinary, obj.Binary);
+                Assert.AreEqual<byte[]>(sproc.TestImage, obj.Image);
+                Assert.AreEqual<Guid>(sproc.TestGuid, obj.Unique);
+            }
         }
     }
 }
