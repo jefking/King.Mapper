96da41a7bae73dcab88fdce61a3c3f1c0a7e5e80
Jef King <jamieking80@gmail.com>
2014-06-20 22:06:41 -0700

    Version Information

---
+++ King.Mapper.Integration/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("King.Mapper.Integration")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Jef King")]
+[assembly: AssemblyProduct("King.Mapper.Integration")]
+[assembly: AssemblyCopyright("Copyright © Jef King 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("4caf61d5-df24-467f-bf61-032e87ac063e")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
