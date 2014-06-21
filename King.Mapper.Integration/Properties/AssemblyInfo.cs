bc781925db52fcadd3be8052553f24c89d8ac896
Jef King <jamieking80@gmail.com>
2014-06-21 16:53:40 -0700

    versions

--- King.Mapper.Integration/Properties/AssemblyInfo.cs
+++ King.Mapper.Integration/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("4caf61d5-df24-467f-bf61-032e87ac063e")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
