81d6dc7cdd3fde868b058662857fa5750e70b3bc
Jef King <jef@agilebusinesscloud.com>
2014-10-01 18:00:57 -0700

    tested dynamics

--- King.Mapper.Integration/DynamicLoaderTests.cs
+++ King.Mapper.Integration/DynamicLoaderTests.cs
@@ -83,6 +83,71 @@
         }
 
         [Test]
+        public async Task DataTableDynamic()
+        {
+            using (var con = new SqlConnection(connectionString))
+            {
+                var sproc = SimulatedSelectStatement.Create();
+
+                var cmd = sproc.Build(con);
+
+                var loader = new DynamicLoader();
+                await con.OpenAsync();
+                var adapter = new SqlDataAdapter(cmd);
+
+                var ds = new DataSet();
+                adapter.Fill(ds);
+                var table = ds.Tables[0];
+                var obj = loader.Dynamic(table);
+
+                Assert.IsNotNull(obj);
+                Assert.AreEqual(sproc.TestInt, obj.Identifier);
+                Assert.AreEqual(sproc.TestBigInt, obj.BigInt);
+                Assert.AreEqual(sproc.TestBit, obj.Bit);
+                Assert.AreEqual(sproc.TestDate.Value.Date, ((DateTime)obj.Date).Date);
+                Assert.AreEqual(sproc.TestDateTime.Value.Date, ((DateTime)obj.DateTime).Date);
+                Assert.AreEqual(sproc.TestDateTime2.Value.Date, ((DateTime)obj.DateTime2).Date);
+                Assert.AreEqual(sproc.TestDecimal, obj.Decimal);
+                Assert.AreEqual(sproc.TestFloat, obj.Float);
+                Assert.AreEqual(Math.Round((decimal)sproc.TestMoney, 4), obj.Money);
+                Assert.AreEqual(sproc.TestNChar, ((string)obj.NChar)[0]);
+                Assert.AreEqual(sproc.TestNText, obj.NText);
+                Assert.AreEqual(sproc.TestText, obj.Text);
+                CollectionAssert.AreEqual(sproc.TestBinary, obj.Binary as byte[]);
+                CollectionAssert.AreEqual(sproc.TestImage, obj.Image as byte[]);
+                Assert.AreEqual(sproc.TestGuid, obj.Unique);
+            }
+        }
+
+        [Test]
+        public async Task DataTableDynamics()
+        {
+            using (var con = new SqlConnection(connectionString))
+            {
+                var sproc = new dboSelectMultipleStatement();
+
+                var cmd = sproc.Build(con);
+
+                var loader = new DynamicLoader();
+                await con.OpenAsync();
+                var adapter = new SqlDataAdapter(cmd);
+
+                var ds = new DataSet();
+                adapter.Fill(ds);
+                var objs = loader.Dynamics(ds.Tables[0]);
+
+                Assert.IsNotNull(objs);
+
+                var i = 0;
+                foreach (var obj in objs)
+                {
+                    Assert.AreEqual(i, obj.Identifier);
+                    i++;
+                }
+            }
+        }
+
+        [Test]
         public async Task DataSetDictionary()
         {
             using (var con = new SqlConnection(connectionString))
