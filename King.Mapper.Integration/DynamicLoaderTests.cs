05b25ee3905f98791e812caaea1a6bde2aa28b2b
Jef King <jef@agilebusinesscloud.com>
2014-10-01 18:22:05 -0700

    Integration tests

--- King.Mapper.Integration/DynamicLoaderTests.cs
+++ King.Mapper.Integration/DynamicLoaderTests.cs
@@ -326,5 +326,69 @@
                 }
             }
         }
+
+        [Test]
+        public async Task DataSetDynamic()
+        {
+            using (var con = new SqlConnection(connectionString))
+            {
+                var sproc = SimulatedSelectStatement.Create();
+
+                var cmd = sproc.Build(con);
+
+                var loader = new DynamicLoader();
+                await con.OpenAsync();
+                var adapter = new SqlDataAdapter(cmd);
+
+                var ds = new DataSet();
+                adapter.Fill(ds);
+                var obj = loader.Dynamic(ds);
+
+                Assert.IsNotNull(obj);
+                Assert.AreEqual(sproc.TestInt, obj.Identifier);
+                Assert.AreEqual(sproc.TestBigInt, obj.BigInt);
+                Assert.AreEqual(sproc.TestBit, obj.Bit);
+                Assert.AreEqual(sproc.TestDate.Value.Date, ((DateTime)obj.Date).Date);
+                Assert.AreEqual(sproc.TestDateTime.Value.Date, ((DateTime)obj.DateTime).Date);
+                Assert.AreEqual(sproc.TestDateTime2.Value.Date, ((DateTime)obj.DateTime2).Date);
+                Assert.AreEqual(sproc.TestDecimal, obj.Decimal);
+                Assert.AreEqual(sproc.TestFloat, obj.Float);
+                Assert.AreEqual(Math.Round((decimal)sproc.TestMoney, 4), obj.Money);
+                Assert.AreEqual(sproc.TestNChar, ((string)obj.NChar)[0]);
+                Assert.AreEqual(sproc.TestNText, obj.NText);
+                Assert.AreEqual(sproc.TestText, obj.Text);
+                CollectionAssert.AreEqual(sproc.TestBinary, obj.Binary as byte[]);
+                CollectionAssert.AreEqual(sproc.TestImage, obj.Image as byte[]);
+                Assert.AreEqual(sproc.TestGuid, obj.Unique);
+            }
+        }
+
+        [Test]
+        public async Task DataSetDynamics()
+        {
+            using (var con = new SqlConnection(connectionString))
+            {
+                var sproc = new dboSelectMultipleStatement();
+
+                var cmd = sproc.Build(con);
+
+                var loader = new DynamicLoader();
+                await con.OpenAsync();
+                var adapter = new SqlDataAdapter(cmd);
+
+                var ds = new DataSet();
+                adapter.Fill(ds);
+                var objs = loader.Dynamics(ds);
+
+                Assert.IsNotNull(objs);
+
+                var i = 0;
+                foreach (var obj in objs)
+                {
+                    Assert.AreEqual(i, obj.Identifier);
+                    i++;
+                }
+            }
+        }
     }
 }
