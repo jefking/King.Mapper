0d02e0d61a4a6b3e8cc817cea531f6c783c90b3c
Jef King <jefkin@microsoft.com>
2016-05-01 14:46:18 -0700

    more better

---
+++ RunTests.bat
@@ -0,0 +1,5 @@
+cd C:\Users\jefkin\Documents\GitHub\King.Mapper\artifacts\bin\King.Mapper.Tests\King.Mapper.Tests\bin\Release\net451
+
+dnvm use 1.0.0-rc1-update1 -r coreclr -a x64
+
+dnu restore -f .
