626441bf5df9f4f17038922b27809d27a6c2565a
Jef King <jefkin@microsoft.com>
2016-08-29 18:57:27 -0700

    workin g

--- King.Mapper.Integration/Configuration.cs
+++ King.Mapper.Integration/Configuration.cs
@@ -2,6 +2,6 @@
 {
     public class Configuration
     {
-        public const string ConnectionString = "Server=tcp:kingmapper.database.windows.net,1433;Initial Catalog=mapperData;Persist Security Info=False;User ID={your_username};Password={your_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+        public const string ConnectionString = "Server=tcp:kingmapper.database.windows.net,1433;Initial Catalog=mapperData;Persist Security Info=False;User ID=jkadmin;Password=$%^&567gfbhj;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
     }
 }
