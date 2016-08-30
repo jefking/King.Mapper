8c13c10bec9914e9552d0eaa24d93ca123fef228
Jef King <jefkin@microsoft.com>
2016-08-29 18:33:24 -0700

    clean up

--- RunTests.ps1
+++
@@ -1,7 +0,0 @@
-CD .\artifacts\bin\King.Mapper.Test.Unit\Release\app
-
-dnvm use 1.0.0-rc1-update1 -r clr -a x64
-dnu restore -f ../../../King.Mapper/Release
-./King.Mapper.Test.Unit.cmd
-
-CD ../../../../../
