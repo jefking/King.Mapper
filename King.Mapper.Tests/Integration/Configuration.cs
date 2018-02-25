09d57c471f6725a8ff69af10c84cf7c9e63d24d0
Jef King <jefkin@microsoft.com>
2018-02-25 08:35:40 -0800

    remove

--- King.Mapper.Tests/Integration/Configuration.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace King.Mapper.Test.Integration
-{
-    public class Configuration
-    {
-        public const string ConnectionString = "Server=tcp:kingmapper.database.windows.net,1433;Initial Catalog=mapperData;Persist Security Info=False;User ID=jkadmin;Password=$%^&567gfbhj;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
-    }
-}
